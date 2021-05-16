f278cdac076e09e04146a5996a0f6493005c99bd
darkmooninja <darkmooninja3481@gmail.com>
2021-05-15 18:04:33 -0700

    Started tower defense
    Added towers and projectiles, customized towers.

---
+++ Assets/Scripts/Scriptables/ProjectileType.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+[CreateAssetMenu(fileName = "Projectile", menuName = "CustomObjects/Projectile")]
+public class ProjectileType : ScriptableObject
+{
+    public int damage;
+    public int speed;
+    public int size;
+    public Color color = Color.black;
+    public Sprite sprite;
+}
